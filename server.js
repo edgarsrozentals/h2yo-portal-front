c8a150198c29f649a9dacda72b106d64e4d61cc3
raiviss <rstudens@gmail.com>
2022-04-27 10:28:02 +0300

    Restructured code

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
 });
 
-app.listen(80);
+app.listen(process.env.PORT || 3000);
+
+console.log('SERVER STARTED. PORT: ', process.env.PORT || 3000);
